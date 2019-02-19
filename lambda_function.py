6a40aa847f1191ea69139bbc1d9635dade8523cb
lsamaha <lwamaha@gmail.com>
2019-02-19 17:33:47 -0500

    updated file download and parse logic by type

--- lambda_function.py
+++ lambda_function.py
@@ -97,6 +97,31 @@ def handle_changed_docs(req_body, dropbox_helper, pdf_helper):
     uploaded_docs = []
     for account, dropbox_doc_paths in dropbox_helper.get_changed(req_body).items():
         for dropbox_doc_path in dropbox_doc_paths:
+            text = None
+            if dropbox_doc_path[-4:] == '.doc':
+                local_doc_path = "/tmp%s" % dropbox_doc_path
+                resp = dropbox_helper.download_file(dropbox_doc_path, local_doc_path)
+                print(resp)
+                with open(local_doc_path, "rb") as f:
+                    data = f.read()
+                    print("downloaded %d bytes" % len(data))
+                    import subprocess
+                    try:
+                        print(subprocess.check_output(['ls', '-l', '/usr/share']))
+                    except Exception as e:
+                        print(e)
+                    try:
+                        print(subprocess.check_output(['mkdir', '-p', '/usr/local/share']))
+                    except Exception as e:
+                        print(e)
+                    try:
+                        print(subprocess.check_output(['cp', '-r', '/opt/share/', '/usr/local/share']))
+                    except Exception as e:
+                        print(e)
+                    try:
+                        text = subprocess.check_output(['antiword', local_doc_path])
+                    except subprocess.CalledProcessError as exc:
+                        print("Status : FAIL", exc.returncode, exc.output)
             if pdf_helper.is_pdf(dropbox_doc_path) and dropbox_doc_paths[-1 * len(__upload_dir):] != __upload_dir:
                 local_doc_path = "/tmp%s" % dropbox_doc_path
                 resp = dropbox_helper.download_file(dropbox_doc_path, local_doc_path)
@@ -105,12 +130,12 @@ def handle_changed_docs(req_body, dropbox_helper, pdf_helper):
                     data = f.read()
                     print("downloaded %d bytes" % len(data))
                 text = pdf_helper.get_text(local_doc_path, resolution=__resolution, max_workers=__max_workers)
-                if text:
-                    __logger.info(text)
-                    __logger.debug("converted doc %s %s" % (account, dropbox_doc_path))
-                    upload_path = "%s%s.txt" % (__upload_dir, ".".join(dropbox_doc_path.split(".")[:-1]))
-                    result = dropbox_helper.store_data(text, upload_path)
-                    print(result)
-                    uploaded_docs.append(upload_path)
+            if text:
+                __logger.info(text)
+                __logger.debug("converted doc %s %s" % (account, dropbox_doc_path))
+                upload_path = "%s%s.txt" % (__upload_dir, ".".join(dropbox_doc_path.split(".")[:-1]))
+                result = dropbox_helper.store_data(text, upload_path)
+                print(result)
+                uploaded_docs.append(upload_path)
     __logger.debug("uploaded docs %s" % uploaded_docs)
     return uploaded_docs
